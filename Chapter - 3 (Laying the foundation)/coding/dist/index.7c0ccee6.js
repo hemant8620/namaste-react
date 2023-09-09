77d8d02988135281eca38249735cec74127862e1
Hemant <hemant.agarwal8620@gmail.com>
2023-09-09 16:24:18 +0530

    push chapter 3

---
+++ Chapter - 3 (Laying the foundation)/coding/dist/index.7c0ccee6.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "I am an H1 tag"),
+        React.createElement("h2", {}, "I am an H2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I am an H1 tag"),
+        React.createElement("h2", {}, "I am an H2 tag")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
